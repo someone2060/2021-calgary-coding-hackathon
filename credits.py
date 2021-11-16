d9f7f59dbef354ad5f0c3b597cc727e4a6c5bff0
someone2060 <74940473+someone2060@users.noreply.github.com>
2021-11-16 14:30:01 -0700

    Update and rename credits.txt to credits.py
    Imported from the replit project

---
+++ credits.py
@@ -0,0 +1,4 @@
+#I made this another file for simplicity and so its easier to read.
+def credits():
+  print("\nThe goal of this program is to help people schedule. Sometimes people will work too much and get stressed or procrastinate and not get anything done. This program has been made to help combat this, so that people will have better mental health.\n We decided to make this program because it can be used by many different people.\n\nThis program was made by Daniel L (someone#6887), Yuilan (Jakeiscake#1913), and Ai Kataoka (Ai Kataoka#6340).\n\nEnter anything to exit.")
+  input()
