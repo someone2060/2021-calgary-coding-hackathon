7c4c9a1344bff370bbdaa2f07bf413a62f11947d
someone2060 <74940473+someone2060@users.noreply.github.com>
2021-11-19 20:38:57 -0700

    Remove TODO list

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-'''
-TODO (in order of importance):
- -Change events in a user friendly manner
- -Have importance levels of events
- -Use something like "import time" and ".sleep()" to make code feel nicer
- -Have a notification notify the user when an event is about to happen (https://stackoverflow.com/questions/12575708/notification-using-python/45090702)
- -Fix the bug of table characters sometimes not loading
- -Allow the user to schedule events that are not on the hour (eg. 18:30-19:15)
- -Have a way for multiple users to use the same application
-'''
 import time
 import data, credits
 
