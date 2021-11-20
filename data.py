4a1580cf332ea19d4b94cd442ab0f7b8f3c51bd9
someone2060 <74940473+someone2060@users.noreply.github.com>
2021-11-19 20:29:53 -0700

    Remove personal data from data.py

--- data.py
+++ data.py
@@ -1,2 +1,2 @@
-name = 'Joe'
-events = [[['SLEEP', 0, 7], ['SLEEP', 23, 24]], [['SLEEP', 0, 7], ['WORK', 9, 17], ['SLEEP', 23, 24]], [['SLEEPEPEPEPEPEP', 0, 7], ['WORK', 9, 17], ['SLEEP', 23, 24]], [['SLEEP', 0, 7], ['WORK', 9, 17], ['SLEEP', 23, 24]], [['SLEEP', 0, 7], ['WORK', 9, 17], ['SLEEP', 23, 24]], [['SLEEP', 0, 7], ['WORK', 9, 17], ['SLEEP', 23, 24]], [['SLEEP', 0, 7], ['SLEEP', 23, 24]]]
+name = ''
+events = [[], [], [], [], [], [], []]
